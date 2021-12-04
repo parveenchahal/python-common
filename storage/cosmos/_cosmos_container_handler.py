fd465e13faa6f00e39b21fd9397883995d179daa
Parveen Chahal <mail.chahal@gmail.com>
2021-12-04 21:47:18 +0000

    used cachetool

--- storage/cosmos/_cosmos_container_handler.py
+++ storage/cosmos/_cosmos_container_handler.py
@@ -1,7 +1,7 @@
-from datetime import datetime, timedelta
-from typing import List, Tuple
-from threading import RLock
+from datetime import timedelta
+from typing import List
 from http import HTTPStatus
+from cachetools import TTLCache, cached
 from azure.core import MatchConditions
 from azure.cosmos import ContainerProxy
 from azure.cosmos.exceptions import CosmosResourceNotFoundError, \
@@ -17,10 +17,6 @@ class CosmosContainerHandler(Storage):
     _database_name: str
     _container_name: str
     _client_builder: CosmosClientBuilderFromKeyvaultSecret
-    _next_read: datetime = None
-    _cache_timeout: timedelta
-    _cached_container_clients: Tuple[ContainerProxy]
-    _lock: RLock
 
     def __init__(
         self,
@@ -31,11 +27,10 @@ class CosmosContainerHandler(Storage):
         self._database_name = database_name
         self._container_name = container_name
         self._client_builder = client_builder
-        self._cache_timeout = cache_timeout
-        self._lock = RLock()
+        self._ttl_cache = TTLCache(10, cache_timeout.total_seconds())
 
     def get(self, id: str, partition_key: str) -> StorageEntryModel:
-        client_list = self._get_clients()
+        client_list = self._get_cached_or_create_clients()
         retries = len(client_list)
         for client in client_list:
             try:
@@ -62,7 +57,7 @@ class CosmosContainerHandler(Storage):
         body['id'] = storage_entry.id
         body['partition_key'] = storage_entry.partition_key
 
-        client_list = self._get_clients()
+        client_list = self._get_cached_or_create_clients()
         retries = len(client_list)
         for client in client_list:
             try:
@@ -79,18 +74,8 @@ class CosmosContainerHandler(Storage):
                 raise exceptions.Unauthorized(e)
             break
 
-    def _update_required(self, now):
-        return self._next_read is None or now >= self._next_read
-
-    def _get_clients(self) -> List[ContainerProxy]:
-        now = datetime.utcnow()
-        if self._update_required(now):
-            with self._lock:
-                if self._update_required(now):
-                    self._cached_container_clients = self._client_builder.get_container_clients(
-                        self._database_name,
-                        self._container_name)
-                    self._next_read = now + self._cache_timeout
-        return self._cached_container_clients
-
-    
+    def _get_cached_or_create_clients(self) -> List[ContainerProxy]:
+        @cached(self._ttl_cache)
+        def wrapper():
+            return self._client_builder.get_container_clients(self._database_name, self._container_name)
+        return wrapper()
