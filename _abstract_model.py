52e16975d40ddd87488bbc526f94ae32447b95e7
Parveen Chahal <mail.chahal@gmail.com>
2022-01-21 19:37:24 +0000

    Update abstract model

--- _abstract_model.py
+++ _abstract_model.py
@@ -1,26 +1,47 @@
+from ast import Mod
 import copy
-from typing import Any
+import json
+from typing import Any, TypeVar
 from dataclasses import dataclass
 from .utils import json_to_obj as _json_to_obj, \
-                    to_json_string as _to_json_string, \
                     dict_to_obj as _dict_to_obj
+
+T = TypeVar('T')
 
 @dataclass
 class Model(object):
 
+    # It must to dict like {'user_id': 'userId'}
+    __mapping__ = None
+
     def to_dict(self, omit_none: bool = False) -> dict:
         d = copy.deepcopy(self.__dict__)
+        d.pop('__mapping__', None)
+        if self.__mapping__ is not None:
+            d = self._parse_dict_with_mapping(self.__mapping__, d)
         if omit_none:
             d = {k: v for k, v in d.items() if v is not None}
         return d
 
-    def to_json_string(self, omit_none: bool = False) -> str:
-        return _to_json_string(self.to_dict(omit_none))
+    def serialize(self, omit_none: bool = False) -> str:
+        return json.dumps(self.to_dict(omit_none))
 
     @staticmethod
-    def from_json_string(cls, json_data: str) -> Any:
-        return _json_to_obj(cls, json_data)
-
-    @staticmethod
-    def from_dict(cls, d: dict) -> Any:
+    def from_dict(cls: T, d: dict) -> T:
+        if cls.__mapping__ is not None:
+            rev_mapping = Model._reverse_mapping(cls.__mapping__)
+            d = Model._parse_dict_with_mapping(rev_mapping, d)
         return _dict_to_obj(cls, d)
+
+    @staticmethod
+    def deserialize(cls: T, json_data: str) -> T:
+        j = json.loads(json_data)
+        return Model.from_dict(cls, j)
+
+    @staticmethod
+    def _parse_dict_with_mapping(mapping: dict, d: dict):
+        return {mapping[k]: v for k,v in d.items()}
+
+    @staticmethod
+    def _reverse_mapping(mapping: dict):
+        return {v: k for k,v in mapping.items()}
